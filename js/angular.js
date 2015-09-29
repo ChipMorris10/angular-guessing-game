86c29d07860faf0c42afd6a1aaa5e7bc8f3af50a
keithhopkins <knhopkins412@gmail.com>
2015-09-29 09:49:59 -0600

    complete guessing game

---
+++ js/angular.js
@@ -0,0 +1,36 @@
+// sample angular code
+
+var app = angular.module('myApp', []);
+
+app.controller('myController', function($scope) {
+  $scope.startGame = function(){
+    $scope.number = Math.floor(Math.random()*100)+1;
+    console.log($scope.number);
+    $scope.showGame = !$scope.showGame;
+    $scope.numGuesses=0;
+    $scope.quitMessage='';
+    $scope.message='';
+  };
+
+  $scope.makeGuess = function(){
+    console.log($scope.guess);
+    $scope.numGuesses++;
+    if($scope.guess < $scope.number){
+      $scope.message = 'Your guess is to low.';
+    } else if($scope.guess > $scope.number){
+      $scope.message = 'Your guess is to high.';
+    } else if($scope.guess === $scope.number){
+      $scope.message = 'You win!';
+    } else {
+      $scope.numGuesses--;
+      $scope.message = 'You guessed outside the range.';
+    }
+    $scope.guess='';
+  };
+
+  $scope.giveUp = function(){
+    $scope.showGame =! $scope.showGame;
+    $scope.quitMessage = "You're a quiter!";
+  }
+
+});
